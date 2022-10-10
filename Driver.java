a34ec5589835a3d7862d1ba133b50a47e80f38a5
David Glick <dglick40@stuy.edu>
2022-10-10 13:20:25 -0400

    Added test for concatArray

--- Driver.java
+++ Driver.java
@@ -6,5 +6,14 @@ public class Driver{
     for (int i= 0; i < ary.length; i++){
       System.out.println(ary[i]==ans[i]);
     }
+    System.out.println("concatArray");
+    int[] ary1 = {1,2,3};
+    int[] ary2 = {4,5,6};
+    int[] shouldAns = {1,2,3,4,5,6};
+    int[] ans1 = arrayConcat.concatArray(ary1,ary2);
+    System.out.println(shouldAns!= ans1);
+    for (int i= 0; i < ary.length; i++){
+      System.out.println(shouldAns[i]==ans1[i]);
+    }
   }
 }
