af4b0c855bc73be6f41f152be34d771a320f77be
David Glick <dglick40@stuy.edu>
2022-10-10 13:14:35 -0400

    Added concatArray

--- arrayConcat.java
+++ arrayConcat.java
@@ -6,4 +6,14 @@ public class arrayConcat{
     }
     return ans;
   }
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] ans = new int[ary1.length+ary2.length];
+    for(int i = 0; i < ary1.length;i++){
+      ans[i]=ary1[i];
+    }
+    for(int i = ary1.length; i < ary1.length+ary2.length;i++){
+      ans[i]= ary2[i-ary1.length];
+    }
+    return ans;
+  }
 }
